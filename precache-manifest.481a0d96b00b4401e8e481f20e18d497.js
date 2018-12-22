13e095352b4ca4a7e9572baa809d0ebb55f74eed
unknown <vandecastro95@gmail.com>
2018-12-21 23:39:08 -0600

    Updates

---
+++ precache-manifest.481a0d96b00b4401e8e481f20e18d497.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "60f0ffb2deb666cfab01",
+    "url": "/ClickyGame/static/js/main.60f0ffb2.chunk.js"
+  },
+  {
+    "revision": "b785a025c4ab1d1a3b44",
+    "url": "/ClickyGame/static/js/1.b785a025.chunk.js"
+  },
+  {
+    "revision": "60f0ffb2deb666cfab01",
+    "url": "/ClickyGame/static/css/main.4db2963a.chunk.css"
+  },
+  {
+    "revision": "77dee2c1096ac933a3eb19665d0f8795",
+    "url": "/ClickyGame/index.html"
+  }
+];
